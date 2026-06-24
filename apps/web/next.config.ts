0d3c0a3bde0dc86a12857c1b1e33173919c82290
yk-graph <yokokura.tatsuya@gmail.com>
2026-06-24 07:00:29 -0700

    add: docker setting in dev, stg, prod

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone',
+  outputFileTracingRoot: path.join(process.cwd(), '../../'),
+}
 
-export default nextConfig;
+export default nextConfig
